2534ec5591ed666472aabdb181cb767c73079b75
Daniher01 <daniher02@gmail.com>
2022-04-25 20:19:20 -0400

    .

--- vista/layouts/header.php
+++ vista/layouts/header.php
@@ -16,7 +16,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="<?php echo constant('URL'); ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="<?php echo constant('URL'); ?>assets/img/logo2.jpg" alt="Logo de recarga" height="60" width="60">
   </div>
 
   <!-- Navbar -->
@@ -57,7 +57,7 @@
   <aside class="main-sidebar sidebar-dark-sp elevation-4">
     <!-- Brand Logo -->
     <div class="brand-link">
-      <img src="<?php echo constant('URL'); ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="<?php echo constant('URL'); ?>assets/img/images.jpg" alt="Logo main" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Servicios Postales</span>
     </div>
 
@@ -66,10 +66,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?php echo constant('URL'); ?>assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?php echo constant('URL'); ?>assets/img/logo usuario2.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block"><?php echo $_SESSION['username'];?></a>
+          <a href="<?php echo constant('URL'); ?>" class="d-block"><?php echo $_SESSION['username'];?></a>
         </div>
       </div>
       
